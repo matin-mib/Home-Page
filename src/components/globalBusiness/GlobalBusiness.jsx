aa34ae2e7036cbf7e02afbc9afe3dafd325a6c0b
matin-mib <matinhasanov.mibtech@gmail.com>
2025-02-07 15:00:41 +0400

    add global business section

---
+++ src/components/globalBusiness/GlobalBusiness.jsx
@@ -0,0 +1,50 @@
+import { Card, Col, Container, Row } from 'react-bootstrap'
+import './globalBusiness.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCreditCard } from '@fortawesome/free-regular-svg-icons'
+
+const GlobalBusiness = () => {
+    return (
+        <Container>
+            <h2 className="global-business__title">Qlobal İş İmkanları</h2>
+            <Row>
+                <Col md={6} className="global-business__column">
+                    <Card className="global-business__card">
+                        <div className="global-business__item">
+                            <FontAwesomeIcon icon={faCreditCard} />
+                            <p className="global-business__text global-business__text--top">
+                                Təhlükəsiz Ödənişlər: Uğurlu Əməkdaşlıqlar Bizimlə Başlayır.
+                            </p>
+                        </div>
+                        <span className="global-business__separator"></span>
+                        <div className="global-business__item">
+                            <FontAwesomeIcon icon={faCreditCard} />
+                            <p className="global-business__text global-business__text--bottom">
+                                Siz ödənişlərdə tam təhlükəsizliyin və şəffaflığın təmin ediodiyindən əmin ola bilərsiniz.
+                            </p>
+                        </div>
+                    </Card>
+                </Col>
+                <Col md={6} className="global-business__column">
+                    <Card className="global-business__card">
+                        <div className="global-business__item">
+                            <FontAwesomeIcon icon={faCreditCard} />
+                            <p className="global-business__text global-business__text--top">
+                                Təhlükəsiz Ödənişlər: Uğurlu Əməkdaşlıqlar Bizimlə Başlayır.
+                            </p>
+                        </div>
+                        <span className="global-business__separator"></span>
+                        <div className="global-business__item">
+                            <FontAwesomeIcon icon={faCreditCard} />
+                            <p className="global-business__text global-business__text--bottom">
+                                Siz ödənişlərdə tam təhlükəsizliyin və şəffaflığın təmin ediodiyindən əmin ola bilərsiniz.
+                            </p>
+                        </div>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default GlobalBusiness
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import Customers from "../components/customers/CustomersReviews"
+import GlobalBusiness from "../components/globalBusiness/GlobalBusiness"
 
 
 const HomePage = () => {
     return (
         <>
             <Customers />
+            <GlobalBusiness />
         </>
     )
 }
